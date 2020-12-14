acc450af2d36da9da431a4406ec317799c506eab
Alexander Ivanov <68683375+AlexanderIvanofff@users.noreply.github.com>
2020-12-14 11:57:26 +0200

    Update HTML_parser.py

--- regular expressions/HTML_parser.py
+++ regular expressions/HTML_parser.py
@@ -1,3 +1,16 @@
+"""
+Write a program that extracts a title of a HTML file and all the content in its body. When you do that print the result in the following format:
+"Title: {extracted title}"
+"Content: {extracted content}"
+The content should be a single string. There might be different tags inside of the body, which you must ignore.
+You extract only the text without the tags. The input will be on a single line. Example: 
+"<html>\n<head><title>News</title></head>\n<body><p><a href="https://softuni.bg">Telerik\nAcademy</a>aims to provide free real-world practical\ntraining 
+for young people who want to turn into\nskillful .NET software engineers.</p></body>\n</html>"
+Here the title is "News" and the content is "Telerik Academy aims to provide free real-world practical training for young people who want to turn into skillful .NET 
+software engineers."
+
+"""
+
 import re
 
 line = input()
