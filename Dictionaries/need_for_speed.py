6a11970332ae6977c95665b4969aacbf5cc578ae
Alexander Ivanov <68683375+AlexanderIvanofff@users.noreply.github.com>
2020-12-16 17:03:41 +0200

    Update need_for_speed.py

--- Dictionaries/need_for_speed.py
+++ Dictionaries/need_for_speed.py
@@ -1,55 +1,71 @@
-n = int(input())
-cars = {}
+def drive(car_dict, current_car, current_distance, current_fuel):
+    if car_dict[current_car]["fuel"] < current_fuel:
+        print(f'Not enough fuel to make that ride')
+    else:
+        car_dict[current_car]["fuel"] -= current_fuel
+        car_dict[current_car]["mileage"] += current_distance
+        print(f'{current_car} driven for {current_distance} kilometers. {current_fuel} liters of fuel consumed.')
+    if car_dict[current_car]["mileage"] >= 100_000:
+        del car_dict[current_car]
+        print(f'Time to sell the {current_car}!')
+    return car_dict
 
+
+def refuel(car_dict, current_car, current_ref):
+    if car_dict[current_car]['fuel'] + current_ref < 75:
+        car_dict[current_car]['fuel'] += current_ref
+        print(f'{current_car} refueled with {current_ref} liters')
+    else:
+        left = 75 - car_dict[current_car]['fuel']
+        print(f'{current_car} refueled with {left} liters')
+        car_dict[current_car]['fuel'] = 75
+    return car_dict
+
+
+def revert(car_dict, current_car, current_kilometers):
+    if car_dict[current_car]["mileage"] - current_kilometers < 10_000:
+        car_dict[current_car]["mileage"] = 10000
+    else:
+        car_dict[current_car]["mileage"] -= current_kilometers
+        print(f'{current_car} mileage decreased by {current_kilometers} kilometers')
+    return car_dict
+
+
+n = int(input())
+
+total_car = {}
 for _ in range(n):
-    car, mileage, fuel = input().split('|')
+    car_name, mileage, fuel = input().split('|')
     mileage = int(mileage)
     fuel = int(fuel)
-    cars[car] = [mileage, fuel]
+
+    total_car[car_name] = {"mileage": mileage, 'fuel': fuel}
 
 
-commands = input()
+command = input()
 
-while not commands == 'Stop':
-    tokens = commands.split(' : ')
-
+while not command == 'Stop':
+    tokens = command.split(' : ')
     instructions = tokens[0]
-    car = tokens[1]
 
     if instructions == 'Drive':
+        car = tokens[1]
         distance = int(tokens[2])
         fuel = int(tokens[3])
-
-        if cars[car][1] <= fuel:
-            print(f'Not enough fuel to make that ride')
-        else:
-            cars[car][0] += distance
-            cars[car][1] -= fuel
-            print(f'{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.')
-
-        if cars[car][0] >= 100_000:
-            print(f'Time to sell the {car}!')
-            del cars[car]
+        total_car = drive(total_car, car, distance, fuel)
 
     elif instructions == 'Refuel':
-        fuel = int(tokens[2])
-        if cars[car][1] + fuel > 75:
-            print(f'{car} refueled with {75 - cars[car][1] } liters')
-            cars[car][1] = 75
-        else:
-            cars[car][1] += fuel
-            print(f'{car} refueled with {fuel} liters')
+        car = tokens[1]
+        ref = int(tokens[2])
+
+        total_car = refuel(total_car, car, ref)
 
     elif instructions == 'Revert':
+        car = tokens[1]
         kilometers = int(tokens[2])
+        total_car = revert(total_car, car, kilometers)
 
-        if cars[car][0] - kilometers < 10_000:
-            cars[car][0] = 10_000
-        else:
-            cars[car][0] -= kilometers
-            print(f'{car} mileage decreased by {kilometers} kilometers')
+    command = input()
 
-    commands = input()
-
-for kay, value in sorted(cars.items(), key=lambda x: (-x[1][0], x[0])):
-    print(f'{kay} -> Mileage: {value[0]} kms, Fuel in the tank: {value[1]} lt.')
+for kay, value in sorted(total_car.items(), key=lambda x: (-x[1]["mileage"], x[0])):
+    print(f'{kay} -> Mileage: {value["mileage"]} kms, Fuel in the tank: {value["fuel"]} lt.')
