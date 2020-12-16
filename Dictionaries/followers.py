05e352df52864bcff76f9f6e33df07d6e2523628
Alexander Ivanov <68683375+AlexanderIvanofff@users.noreply.github.com>
2020-12-16 17:00:48 +0200

    Update followers.py

--- Dictionaries/followers.py
+++ Dictionaries/followers.py
@@ -1,34 +1,65 @@
+def new_follower(name_information_dict, current_username):
+    if current_username not in name_information_dict:
+        name_information_dict[current_username] = {'like': 0, 'comments': 0}
+    return name_information_dict
+
+
+def like(name_information_dict, current_username, current_count):
+    if current_username in name_information_dict:
+        name_information_dict[current_username]['like'] += current_count
+    else:
+        name_information_dict[current_username] = {"like": current_count, 'comments': 0}
+    return name_information_dict
+
+
+def comment(name_information_dict, current_username):
+    if current_username not in name_information_dict:
+        name_information_dict[current_username] = {"like": 0, 'comments': 1}
+    else:
+        name_information_dict[current_username]["comments"] += 1
+    return name_information_dict
+
+
+def blocked(name_information_dict, current_username):
+    if current_username in name_information_dict:
+        del name_information_dict[current_username]
+    else:
+        print(f'{current_username} doesn\'t exist.')
+    return name_information_dict
+
+
+name_information = {}
+
 command = input()
-followers = {}
+
 while not command == 'Log out':
-    tokens = command.split(': ')
+    tokens = command.split(": ")
     instructions = tokens[0]
-    username = tokens[1]
 
     if instructions == 'New follower':
-        if username not in followers:
-            followers[username] = [0, 0]
+        username = tokens[1]
+
+        name_information = new_follower(name_information, username)
 
     elif instructions == 'Like':
+        username = tokens[1]
         count = int(tokens[2])
-        if username in followers:
-            followers[username][0] += count
-        else:
-            followers[username] = [count, 0]
+
+        name_information = like(name_information, username, count)
 
     elif instructions == 'Comment':
-        if username in followers:
-            followers[username][1] += 1
-        else:
-            followers[username] = [0, 1]
+        username = tokens[1]
+        name_information = comment(name_information, username)
 
     elif instructions == 'Blocked':
-        if username in followers:
-            del followers[username]
-        else:
-            print(f'{username} doesn\'t exist')
+        username = tokens[1]
+        name_information = blocked(name_information, username)
 
     command = input()
-print(f'{len(followers)} followers')
-for kay, value in sorted(followers.items(), key=lambda x: (-x[1][0], x[0])):
-    print(f'{kay}: {sum(value)}')
+
+
+print(f'{len(name_information)} followers')
+
+for kay, value in sorted(name_information.items(), key=lambda x: (-x[1]['like'] + x[1]["comments"], x[0])):
+    total_likes_and_commends = value["like"] + value["comments"]
+    print(f'{kay}: {total_likes_and_commends}')
